f41fb8496490c4834b88ae11e1abf31d3960b4ea
Steve Brownlee <chortlehoort@gmail.com>
2017-09-18 09:55:41 -0500

    Capturing user name and address on registration

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,10 +8,17 @@ namespace Bangazon.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser()
+        {
+             
+        }
+
         [Required]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
 
         [Required]
